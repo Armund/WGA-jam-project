fc06f6aa9e15fb76d87530c8f917f171a08ef3cb
Nikita_G <nikita.gorlyak@yandex.ru>
2020-12-05 21:16:44 +0300

    With new camera feels like different game....

---
+++ WGA-jam-project/Assets/_Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed;
+
+    public Vector3 offset;
+
+    protected void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}

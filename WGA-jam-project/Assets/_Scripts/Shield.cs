2546bb2a5fc097c339c378aacfec53ad57a86b88
Armund <erm_alex@list.ru>
2020-12-05 13:42:15 +0300

    Basic shield

---
+++ WGA-jam-project/Assets/_Scripts/Shield.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+	public GameObject shieldModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Activate() {
+		shieldModel.SetActive(true);
+	}
+
+	public void Disactivate() {
+		shieldModel.SetActive(false);
+	}
+
+}

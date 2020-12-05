96e7ea3781debb35308881baa9a4165cfdeb2326
Armund <erm_alex@list.ru>
2020-12-05 19:44:24 +0300

    basic SoundManager

---
+++ WGA-jam-project/Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public enum Sounds {MAIN_THEME, SHOOT, SHIELD_REFLECTION, SHIELD_UP, SHIELD_DOWN, EXPLOSION, WALK }
+
+	public static SoundManager instance = null;
+
+	public AudioSource[] audioSources = new AudioSource[20];
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	public void PlaySound(Sounds sound) {
+		audioSources[(int)sound].Play();
+	}
+
+	public void StopSound(Sounds sound) {
+		audioSources[(int)sound].Stop();
+	}
+}

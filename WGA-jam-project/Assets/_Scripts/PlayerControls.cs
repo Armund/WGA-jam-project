2b1b8fc86fa269c585c183a24528650650e1d99e
SteshenkoSv <bigslav98@gmail.com>
2020-12-05 19:40:44 +0300

    Collisions

--- WGA-jam-project/Assets/_Scripts/PlayerControls.cs
+++ WGA-jam-project/Assets/_Scripts/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/_Scripts/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
